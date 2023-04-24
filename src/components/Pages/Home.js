815020af6df13567408b56b13151512c1fcc4f76
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-04-24 16:12:01 +0700

    update news

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -4,12 +4,26 @@ import video from '../../asset/videos/background-video.mp4';
 import ngt from '../../asset/images/ngtpresent.png'
 import '../../styles/css/Home.css';
 import { useTranslation } from 'react-i18next';
+import thelastestspan from '../../asset/images/thelastestspan1.png';
+import card01logo from '../../asset/images/Card01-logo.png';
+import card01bg from '../../asset/images/card01-bg.png';
+import card01character from '../../asset/images/card01-character.png';
+import card02bg from '../../asset/images/card02-bg.png';
+import card02logo from '../../asset/images/card02-logo.png';
+import card02character from '../../asset/images/card02-character.png';
+import card03bg from '../../asset/images/card03-bg.png';
+import card03logo from '../../asset/images/card03-logo.png';
+import card03character from '../../asset/images/card03-character.png';
+import card04bg from '../../asset/images/card04-bg.png';
+import card04logo from '../../asset/images/card04-logo.png';
+import card04character from '../../asset/images/card04-character.png';
 
 export default function Home() {
   const { t } = useTranslation();
 
   return (
     <>
+      {/* Email Container */}
       <div className='home-module-video' style={{ position: 'relative' }}>
         <video preload="true" muted loop playsInline data-testid="hero:video" autoPlay style={{ width: '100%', height: '100%' }}>
           <source src={video} type="video/mp4" />
@@ -26,6 +40,65 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* Body 1  */}
+      <div className='body-01-container container-fluid text-white'>
+        <hr class="hr-text" data-content="NEWS" />
+        <div className='body-01 row p-5'>
+          <div className='col-2'></div>
+          <div className='col-8 text-center'>
+          <img className='ms-5' src={thelastestspan} height={115} />
+          </div>
+          <div className='col-2'></div>
+        </div>
+        <div className='body-02 row'>
+          <div className='col-2'></div>
+          <div className='col-2'>
+            <a href='https://www.youtube.com/watch?v=xjhQOX3B9tU&ab_channel=PlayStation' target='_blank' rel="noreferrer">
+              <div class="card">
+                <div class="wrapper">
+                  <img src={card01bg} class="cover-image" />
+                </div>
+                <img src={card01logo} class="title-1" />
+                <img src={card01character} class="character" />
+              </div>
+            </a>
+          </div>
+          <div className='col-2'>
+          <a href='https://www.youtube.com/watch?v=pyC_qiW_4ZY&ab_channel=PlayStation' target='_blank' rel="noreferrer">
+            <div class="card">
+              <div class="wrapper">
+                <img src={card02bg} class="cover-image" />
+              </div>
+              <img src={card02logo} class="title-1" />
+              <img src={card02character} class="character" />
+            </div>
+            </a>
+          </div>
+          <div className='col-2'>
+            <div class="card">
+              <div class="wrapper">
+                <img src={card03bg} class="cover-image" />
+              </div>
+              <img src={card03logo} class="title-1" />
+              <img src={card03character} class="character" />
+            </div>
+          </div>
+          <div className='col-2'>
+            <div class="card">
+              <div class="wrapper">
+                <img src={card04bg} class="cover-image" />
+              </div>
+              <img src={card04logo} class="title-1" />
+              <img src={card04character} class="character" />
+            </div>
+          </div>
+          <div className='col-2'></div>
+        </div>
+        <br /> <br />
+        <hr class="hr-text mt-5" data-content="CONCEPTS" />
+
+      </div >
     </>
   );
 }
