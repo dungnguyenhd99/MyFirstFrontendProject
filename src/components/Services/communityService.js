a1e002dc2cf8112efa24dac09f66af41e80d23b8
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-07-21 18:09:27 +0700

    update

--- src/components/Services/communityService.js
+++ src/components/Services/communityService.js
@@ -1,15 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
 
-const API_URL = "localhost:3000/friendship/";
-
 class CommunintyService {
     getFriendList(access_token, search) {
         const headers = {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${access_token}`
         };
-        return axios.get(API_URL + `friend-list?search=${search}`, { headers })
+        return axios.get('https://ngtbackend-production.up.railway.app/auth/' + `friend-list?search=${search}`, { headers })
     }
 }
 
